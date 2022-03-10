0eab0fcafa87cf42579744d393cbece76fafb621
Marcelo Junior <marcelohjw@gmail.com>
2022-03-10 13:26:01 -0300

    Double screen implemented with miau button

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import MainScreen from './screens/MainScreen';
 
 export default function App() {
+  const [screen, setScreen] = useState(true);
+
+  const modificarHandle = () => {
+    if (screen) {
+      setScreen(false);
+    } else {
+      setScreen(true);
+    }
+  };
   return (
     <View style={styles.container}>
-        <MainScreen>
-          <Text>Miau</Text>
-        </MainScreen>
+        {screen ? (
+          <MainScreen>
+            <Text>Miau</Text>
+          </MainScreen>
+        ) : (
+          <Text>Segunda tela</Text>
+        )}
+        <View>
+          <Button title='Miau' onPress={modificarHandle}/>
+        </View>
     </View>
   );
 }
